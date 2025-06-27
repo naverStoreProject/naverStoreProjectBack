947f6f4c2e6593351a24612e861f872864a4d6ec
winA <wina418@kw.ac.kr>
2025-06-28 00:02:35 +0900

    feat: DTO 작성 #26

--- src/main/java/com/cloneproject/demo/order/Order.java
+++ src/main/java/com/cloneproject/demo/order/Order.java
@@ -1,6 +1,50 @@
 //도메인
 package com.cloneproject.demo.order;
 
-public class Order {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Date;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+
+@Data
+@NoArgsConstructor
+public class Order {
+	
+	private Long id;
+	private Long orderGroupId;
+	private String title;
+	private String detail;
+	private String image;
+	private Integer count;
+	private Integer price;
+	private String link;
+	private LocalDateTime orderTime;
+	private OrderEnum status;
+	private LocalDateTime statusChangedTime;
+	private String company;
+	
+	public Order(Long id, Long orderGroupId, String title, String detail, String image, Integer count,
+			Integer price, String link, LocalDateTime orderTime, OrderEnum status,
+			LocalDateTime statusChangedTime,
+			String company) {
+		this.id = id;
+		this.orderGroupId = orderGroupId;
+		this.title = title;
+		this.detail = detail;
+		this.image = image;
+		this.count = count;
+		this.price = price;
+		this.link = link;
+		this.orderTime = orderTime;
+		this.status = status;
+		this.statusChangedTime = statusChangedTime;
+		this.company = company;
+	}
 }
--- src/main/java/com/cloneproject/demo/order/OrderGroup.java
+++ src/main/java/com/cloneproject/demo/order/OrderGroup.java
@@ -1,6 +1,25 @@
 //도메인
 package com.cloneproject.demo.order;
 
-public class OrderGroup {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Data
+@NoArgsConstructor
+public class OrderGroup {
+	
+	private Long id;
+	private Long memberId;
+	
+	public OrderGroup(Long id, Long memberId) {
+		this.id = id;
+		this.memberId = memberId;
+	}
 }
